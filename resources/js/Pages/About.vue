01a91500f6be218cb47bb76a9f76cbe52c27c094
Pablo Nogueira <pablonogueira1996@gmail.com>
2024-10-15 22:58:50 -0300

    The true first commit

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+defineProps({
+    user: String
+})
+</script>
+
+<template>
+
+    <Head title="About" />
+    <h1 class="text-2xl font-bold font-mono">About page</h1>
+    <h3>Now we're talking about {{ user }}</h3>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    user: String
+})
+</script>
+
+<template>
+
+    <Head title="Home" />
+    <h1>Hello, Universe</h1>
+    <h3>Now we're talking about {{ user }}</h3>
+</template>
